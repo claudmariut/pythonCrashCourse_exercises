06718abe91f1d20b7b30c535512d294384a8509f
claumariut <mclaudpython@gmail.com>
2020-09-09 19:11:56 -0500

    import classes

--- Chapter 9. Classes/car.py
+++ Chapter 9. Classes/car.py
@@ -1,4 +1,6 @@
-# Working with Classes and instances.
+"""A class that can be used to represent a gas and electric cars."""
+
+
 class Car:
     """A simple attempt to represent a car"""
 
@@ -81,28 +83,6 @@ class ElectricCar(Car):
         print("This car does not need a gas tank!")
 
 
-my_new_car = Car("subaru", "outback", 2015)
-print(my_new_car.get_descriptive_name())
-
-my_new_car.update_odometer(23500)
-my_new_car.read_odometer()
-
-my_new_car.increment_odometer(100)
-my_new_car.read_odometer()
-
-# Create instance for the child class.
-my_tesla = ElectricCar("tesla", "model s", 2019)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-
-my_new_car.gas_tank_size(65)
-my_tesla.gas_tank_size(23)  # We override the method.
-print(my_tesla.sizeTank)
-print(my_new_car.sizeTank)
-
-my_tesla.battery.get_range()
-my_tesla.battery.upgrade_battery()
-my_tesla.battery.get_range()
 
 
 
